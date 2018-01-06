139c2f9c22958687d405633ec894e2916c4749a3
Ildar Saidashev <isaidashev@gmail.com>
2018-01-06 11:58:17 +0300

    Move HW 5 files in VPN

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ~
+git clone https://github.com/Otus-DevOps-2017-11/reddit.git
+cd ~/reddit
+bundle install
+puma -d
+if ps aux | grep puma | grep -vq grep; then
+  echo "Puma start"
+else
+  echo "Puma stop"
+fi
